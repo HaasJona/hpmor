d1ec14929a9944873bfc72b76506c89d714a140d
Jonathan Haas <jonathan.haas@kappich.de>
2017-12-07 09:16:39 +0100

    Renamed Files

--- pack.sh
+++ pack.sh
@@ -2,10 +2,8 @@
 
 rm 'Harry Potter and the Methods of Rationality.epub'
 
-zip -r 'Harry Potter and the Methods of Rationality.epub' *
+zip -r 'Harry Potter and the Methods of Rationality.epub' mimetype OEBPS META-INF
 
 sigil 'Harry Potter and the Methods of Rationality.epub'
 
-unzip 'Harry Potter and the Methods of Rationality.epub'
-
-rm 'Harry Potter and the Methods of Rationality.epub'
+unzip -o 'Harry Potter and the Methods of Rationality.epub'
